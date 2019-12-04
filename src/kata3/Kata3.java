840a2061a3acca61a3c29bc2cfcd644d3f541df6
carlo <carlo@DESKTOP-3R0ABNQ>
2019-12-04 10:11:21 +0000

    Kata 3

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,7 +3,15 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        new HistogramDisplay();
+        Histogram<String> histogram = new Histogram<>();
+        
+        histogram.increment("mail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("dis.ulpgc.es");
+        
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
 }
